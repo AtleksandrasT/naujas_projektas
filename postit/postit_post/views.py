1f29eca81ca0e33e58373514e8963af430aa352c
AtleksandrasT <Aleksandras.tubis@gmail.com>
2022-07-11 11:56:11 +0300

    urls_view_serializers

--- postit/postit_post/views.py
+++ postit/postit_post/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from . import models, serialazers
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serialazers.PostSerializer
+    premission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
